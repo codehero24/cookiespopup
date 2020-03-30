df3d36ca86d3c6818f7d3375fa35fbe8634dc186
codehero24 <62588088+codehero24@users.noreply.github.com>
2020-03-30 15:52:23 +0500

    Create cookiespopupfile.js

---
+++ cookiespopupfile.js
@@ -0,0 +1,18 @@
+<script>
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#0373b9"
+    },
+    "button": {
+      "background": "#e74c3c"
+    }
+  },
+  "theme": "edgeless",
+  "content": {
+    "dismiss": "Got it! Thanks.",
+    "link": "Learn More.",
+    "href": "https://neutrinotestsite.blogspot.com/"
+  }
+});
+</script>
